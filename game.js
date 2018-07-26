21fd51e0c8d117fc5695495de2b56dc1b3c9403a
SammyAwesome <sammyhart26@gmail.com>
2018-07-25 22:18:09 -0700

    Update 0.1
    PertArt is beginning.
    (dun dun dun)

--- game.js
+++ game.js
@@ -32,33 +32,22 @@ function onKeyStart(key) {
 
 // Called 30 times or more per second
 function onTick() {
-    // Some sample drawing 
+    // Some sample drawing
+    fillRectangle(0,0,screenWidth,screenHeight, makeColor(1,1,1))
+    fillRectangle(0,0,220,80, makeColor(.8,.9,1))
 
-    clearRectangle(0, 0, screenWidth, screenHeight);
-
-    fillText("hello world",
-             screenWidth / 2, 
-             screenHeight / 2,             
-             makeColor(0.5, 0.0, 1.0, 1.0), 
-             "300px Times New Roman", 
-             "center", 
-             "middle");
-
-    fillText(round(currentTime() - START_TIME) + " seconds since start",
-             screenWidth / 2, 
-             screenHeight / 2 + 300,   
-             makeColor(1.0, 1.0, 1.0, 1.0), 
-             "100px Arial", 
-             "center", 
-             "middle");
-
-    fillText("last key code: " + lastKeyCode,
-             screenWidth / 2, 
-             screenHeight / 2 + 500,             
-             makeColor(0.7, 0.7, 0.7, 1.0), 
-             "100px Arial", 
-             "center", 
-             "middle");
+    strokeLine(screenWidth - (895/2), 0, screenWidth-(895/2),screenWidth-900,  makeColor(0,0,0), 6)
+    strokeRectangle(895,0,screenWidth-900,screenWidth-900, makeColor(0,.9,.9), 10)
+    fillText("Pert",
+             20,
+             55,
+             makeColor(0, 0, 0),
+             "50px Arial");
+             fillText("Art",
+                      110,
+                      55,
+                      makeColor(.5, 0, 1),
+                      "50px Arial");
 }
 
 
